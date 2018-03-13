0c121fb28ad54e2cda21528e1fbf44bc07246c16
NB\suao9 <60211tom@gmail.com>
2018-03-13 22:03:39 +0800

    test google script

--- index.php
+++ index.php
@@ -4,43 +4,57 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if($method == 'POST'){
-	$requestBody = file_get_contents('php://input');
-	$responseSet = file_get_contents('response/response.json');
-	$json = json_decode($requestBody);
-	$responseSetJson = json_decode($responseSet);
-	$questionType = $json->result->parameters->questionType;
-	$speech = "sorry";
+    $requestBody = file_get_contents('php://input');
+    $responseSet = file_get_contents('response/response.json');
+    $json = json_decode($requestBody);
+    $responseSetJson = json_decode($responseSet);
+    $questionType = $json->result->parameters->questionType;
+    $anyText = $json->result->parameters->any;
+    $speech = "sorry";
 
-    $url = 'https://script.google.com/macros/s/AKfycbxwZtpnWeyD0ar-rvQCp5OMk_Dq7F0ST-5p41EIvGt_OFflh6Q1/exec?i=' . $questionType[0];
-    $content = file_get_contents($url);
+    $url = 'https://script.google.com/macros/s/AKfycbxwZtpnWeyD0ar-rvQCp5OMk_Dq7F0ST-5p41EIvGt_OFflh6Q1/exec?i=' . $anyText[0];
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $contents = curl_exec($ch);
+    curl_close($ch);
+    $contents = htmlentities($contents,ENT_QUOTES,"UTF-8");
+    $k = strpos($contents,"userHtml");
+    $contents = substr($contents, $k+17);
+    $k = strpos($contents,"x22");
+    $contents = substr($contents, 0,$k-1);
 
+    preg_match("/<title>(.*)<\/title>/s", $content, $match);
+    $title = $match[1];
     $responseCount = count($responseSetJson->imageUrl);
     $image = $responseSetJson->imageUrl[rand(0, $responseCount - 1)];
 
     $messages = array(
         array(
-			"type" => 0,
-			"platform" => "facebook",
-			"speech" => "provide " .$questionType[0] . ". server meow meow"
-    	),
-		array(
-			"type" => 3,
-			"platform" => "facebook",
-			"imageUrl" => $image
-		)
+            "type" => 3,
+            "platform" => "facebook",
+            "imageUrl" => $contents
+        ),
+        array(
+            "type" => 3,
+            "platform" => "facebook",
+            "imageUrl" => $image
+        )
     );
 
 
-	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
-	$response->messages = $messages;
-	echo json_encode($response);
+    $response = new \stdClass();
+    $response->speech = $speech;
+    $response->displayText = $speech;
+    $response->source = "webhook";
+    $response->messages = $messages;
+    echo json_encode($response);
 }
 else
 {
-	echo "Method not allowed";
+    echo "Method not allowed";
 }
 
 ?>
