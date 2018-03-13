2517a23af234ad2f74fc17a19148e8d91ebb2be0
NB\suao9 <60211tom@gmail.com>
2018-03-13 12:51:25 +0800

    add random image

--- index.php
+++ index.php
@@ -5,23 +5,31 @@ $method = $_SERVER['REQUEST_METHOD'];
 // Process only when method is POST
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
+	$responseSet = file_get_contents('response/response.json');
 	$json = json_decode($requestBody);
-
+	$responseSetJson = json_decode($responseSet);
 	$questionType = $json->result->parameters->questionType;
 	$speech = "sorry";
 
+    $url = 'https://script.google.com/macros/s/AKfycbxwZtpnWeyD0ar-rvQCp5OMk_Dq7F0ST-5p41EIvGt_OFflh6Q1/exec?i=' . $questionType[0];
+    $content = file($url);
+
+    $responseCount = count($responseSetJson->imageUrl);
+    $image = $responseSetJson->imageUrl[rand(0, $responseCount - 1)];
+
     $messages = array(
-            array(
-                "type" => 3,
-                "platform" => "facebook",
-                "imageUrl" => "https://i.imgur.com/AD3MbBi.jpg"
-            ),
-            array(
-                "type" => 0,
-                "platform" => "facebook",
-                "speech" => "server response"
-			)
+        array(
+        "type" => 0,
+        "platform" => "facebook",
+        "speech" => "sever response"
+    	),
+        array(
+            "type" => 3,
+            "platform" => "facebook",
+            "imageUrl" => $image
+        )
     );
+
 
 	$response = new \stdClass();
 	$response->speech = $speech;
