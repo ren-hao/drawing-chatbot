a88a9e237ea15585d2d86cbc9756c6d92fd1cfb5
NB\suao9 <60211tom@gmail.com>
2018-03-22 21:25:04 +0800

    jsonVersion

--- index.php
+++ index.php
@@ -9,46 +9,17 @@ if($method == 'POST'){
 	$json = json_decode($requestBody);
 	$responseSetJson = json_decode($responseSet);
 	//$questionType = $json->result->parameters->questionType;
-	$anyText = $json->result->resolvedQuery;	
 
-	$ctext = urlencode($anyText);
+    $imageCount = count($responseSetJson->imageUrl);
+	$textCount = count($responseSetJson->text);
+    $image = $responseSetJson->imageUrl[rand(0, $imageCount - 1)];
+	$text = $responseSetJson->text[rand(0, $textCount - 1)];
 
-    $url = 'https://script.google.com/macros/s/AKfycbxwZtpnWeyD0ar-rvQCp5OMk_Dq7F0ST-5p41EIvGt_OFflh6Q1/exec?i=' . $ctext;
-
-	$parameter = "php(parameter)";
-	exec("python test.py ".$parameter, $output);
-	// $output[0] 是python回傳的'
-	
-    $ch = curl_init();
-    $timeout = 0;
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-    $contents = curl_exec($ch);
-    curl_close($ch);
-    $contents = htmlentities($contents,ENT_QUOTES,"UTF-8");
-    
-	
-    $tp = $contents;	
-    $k = strpos($contents,"userHtml");
-    $contents = substr($contents, $k+17);
-    $k = strpos($contents,"x22");
-    $contents = substr($contents, 0,$k-1);
-    $decodecontents = urldecode($contents);
-	
-    $responseCount = count($responseSetJson->imageUrl);
-    $image = $responseSetJson->imageUrl[rand(0, $responseCount - 1)];
-
-    $messages = array(
+	$messages = array(
         array(
 			"type" => 0,
 			"platform" => "facebook",
-			"speech" => $output[0]
-    	),	
-        array(
-			"type" => 0,
-			"platform" => "facebook",
-			"speech" => $decodecontents
+			"speech" => $text
     	),
 		array(
 			"type" => 3,
@@ -59,8 +30,6 @@ if($method == 'POST'){
 
 
 	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
 	$response->source = "webhook";
 	$response->messages = $messages;
 	echo json_encode($response);
--- test.py
+++
@@ -1,9 +0,0 @@
- #-*- coding: utf-8 -*-
-import warnings
-import sys
-
-def main():
-        query = sys.argv[1:]
-        print("this is python " + query[0])
-if __name__ == "__main__": 
-    main()
